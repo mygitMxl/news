63a955bba2df7660eabedb7e4e87fbee2d6be7ab
mygitMxl <1065617571@qq.com>
2023-01-10 11:45:29 +0800

    home布局与数据展示

--- src/views/sandbox/home/home.js
+++ src/views/sandbox/home/home.js
@@ -1,7 +1,76 @@
-import React from 'react'
+import { Card, Col, Row, List,Avatar } from 'antd'
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import React,{useEffect,useState} from 'react'
+import axios from 'axios';
+const { Meta } = Card;
 
-export default function home() {
+export default function Home() {
+  const [viewList, setviewList] = useState([])
+  const [starList, setstarList] = useState([])
+  const{username,region,role:{roleName}}=JSON.parse(localStorage.getItem('token'))
+  useEffect(()=>{/* _sort:正序,_order=desc反序,_limit限制条数*/
+    axios.get("/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6").then(res=>{
+        // console.log(res.data)
+        setviewList(res.data)
+    })
+},[])
+useEffect(() => {
+  axios.get('/news?publishState=2&_expand=category&_sort=star&_order=desc&_limit=6')
+  .then(res=>{
+    console.log(res.data);
+    setstarList(res.data)
+  })
+}, [])
+
   return (
-    <div>home</div>
+    <div className="site-card-wrapper">
+      <Row gutter={16}>
+        <Col span={8}>
+         <Card title={'用户最常浏览'} bordered={true}>
+          <List
+           size='small'
+           dataSource={viewList}
+           renderItem={item => <List.Item>
+            <a href={`#/news-manage/preview/${item.id}`}>{item.title}</a>
+        </List.Item>}
+    />
+         </Card>
+        </Col>
+        <Col span={8}>
+         <Card title={'用户最常浏览'} bordered={true}>
+          <List
+           size='small'
+           dataSource={starList}
+           renderItem={item => <List.Item>
+            <a href={`#/news-manage/preview/${item.id}`}>{item.title}</a>
+        </List.Item>}
+    />
+         </Card>
+        </Col>
+        <Col span={8}>
+         <Card
+         cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" style={{width:'100%'}}/>}
+         actions={[
+          <SettingOutlined key="setting" />,
+          <EditOutlined key="edit" />,
+          <EllipsisOutlined key="ellipsis" />,
+        ]}
+         >
+         <Meta
+        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+        title={username}
+         description={
+         <div>
+          <b>{region?region:'全球'}</b>
+          <span style={{paddingLeft:'30px'}}>
+            {roleName}
+          </span>
+         </div>
+         }
+    />
+         </Card>
+        </Col>
+      </Row>
+    </div>
   )
 }
