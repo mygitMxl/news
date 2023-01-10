a4436add5311218efb964b675f1e8ad45ee0291e
mygitMxl <1065617571@qq.com>
2023-01-10 21:43:01 +0800

    柱状图和优化

--- src/views/sandbox/home/home.js
+++ src/views/sandbox/home/home.js
@@ -1,16 +1,17 @@
 import { Card, Col, Row, List,Avatar } from 'antd'
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useRef,useState} from 'react'
 import axios from 'axios';
+import * as echarts from 'echarts';
+import _ from 'lodash'
 const { Meta } = Card;
-
 export default function Home() {
   const [viewList, setviewList] = useState([])
   const [starList, setstarList] = useState([])
+  const barRef = useRef()
   const{username,region,role:{roleName}}=JSON.parse(localStorage.getItem('token'))
   useEffect(()=>{/* _sort:正序,_order=desc反序,_limit限制条数*/
     axios.get("/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6").then(res=>{
-        // console.log(res.data)
         setviewList(res.data)
     })
 },[])
@@ -22,6 +23,51 @@ useEffect(() => {
   })
 }, [])
 
+/* ................................................ */
+useEffect(() => {
+  axios.get('/news?publishState=2&_expand=category')
+  .then(res=>{
+    console.log(res.data);
+    renderBarView(_.groupBy(res.data,item=>item.category.title))/* groupBy 按照第二个参数分组 */
+  })
+  return()=>{
+    window.resize=null
+  }
+}, [])
+const renderBarView=(obj)=>{
+  console.log(obj);
+  var myChart = echarts.init(barRef.current);
+  var option = {
+    title: {
+      text: '新闻分类图示'
+    },
+    tooltip: {},
+    legend:{
+        data:['数量'] 
+    },
+    xAxis: {
+      data: Object.keys(obj),
+      axisLabel:{
+        rotate:"45",/* 旋转45度 */
+        interval:0
+    }
+    },
+    yAxis: {
+      minInterval: 1 /* 最小间隔为1 */
+    },
+    series: [
+      {
+        name: '数量',
+        type: 'bar',
+        data: Object.values(obj).map(item=>item.length)
+      }
+    ]
+};
+myChart.setOption(option);
+window.onresize=()=>{/* 窗口改变事件触发 */
+  myChart.resize() /* 将内容变成响应式的 */
+}
+}
   return (
     <div className="site-card-wrapper">
       <Row gutter={16}>
@@ -71,6 +117,9 @@ useEffect(() => {
          </Card>
         </Col>
       </Row>
+      <div ref={barRef} style={{width:'100%',height:'400px',marginTop:'30px'}}>
+  
+      </div>
     </div>
   )
 }
