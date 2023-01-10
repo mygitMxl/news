e346219d2e7119dfcb02674594a1be0b102fcb61
mygitMxl <1065617571@qq.com>
2023-01-10 23:30:03 +0800

    游客系统路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom'
 import Login from '../views/Login/index'
 import NewSandBox from '../views/sandbox/NewSandBox'
+import News from '../views/news/News'
+import Detail from '../views/news/Detail'
 export default function index() {
   return (
-
         <HashRouter>
             <Switch>
            <Route path={'/login'} component={Login}/>
+           <Route path="/news" component={News}/>
+          <Route path="/detail/:id" component={Detail}/>
           <Route path={'/'}render={()=>{
             return localStorage.getItem('token')?<NewSandBox/>:<Redirect to={'/login'}/>
           }}/>
+         
           </Switch>
         </HashRouter>
    
